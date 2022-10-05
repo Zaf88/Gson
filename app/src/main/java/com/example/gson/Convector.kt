aa94ff1a456f206e35521e649ee80c04f0e77a97
Your Name <applezafar@gmail.com>
2022-10-05 16:13:37 +1000

    закомитил лог, подчеркивает.

--- app/src/main/java/com/example/gson/Convector.kt
+++ app/src/main/java/com/example/gson/Convector.kt
@@ -6,20 +6,20 @@ import com.google.gson.Gson
 import com.google.gson.JsonObject
 
 class Convector {
-//    Конвертация body-object в формат Json
+
     val gson = Gson()
 
-        private val request = gson.toJson(JsonObject().apply {
-            addProperty("count", 5)
-            addProperty("country", "ru")
-            addProperty("list", gson.toJson(listOf("123", "1234567", "7654321")))
-        })
+    private val request = gson.toJson(JsonObject().apply {
+        addProperty("count", 5)
+        addProperty("country", "ru")
+        addProperty("list", gson.toJson(listOf("1", "2", "3")))
+    })
 
+//    Log.e("LOG=>",request)
 
-//        Конвертация  формат Json в data class
-private val response = "{\"count\":5,\"country\":\"ru\",\"list\":[\"123\",\"1234567\",\"7654321\"]}"
+    private val response = "{\"count\":5,\"country\":\"ru\",\"list\":[\"123\",\"1234567\"]}"
 
-       private val model=gson.fromJson(response, SampleModel::class.java)
+    private val model = gson.fromJson(response, SampleModel::class.java)
 
 
 }
